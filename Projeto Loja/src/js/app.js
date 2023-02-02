6cfd7e3f37b2d9c0ef6f351af3717a645c9a0bab
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-02-02 10:34:09 -0300

    subindo funções de slide no js

---
+++ Projeto Loja/src/js/app.js
@@ -0,0 +1,35 @@
+/*Slide imagem*/
+
+let atual = 0;
+let images = document.querySelectorAll('.slides .imgs');
+let firstImg = document.getElementById('primeiraImg');
+
+document.getElementById('bt-voltar').addEventListener('click', function () {
+    atual--;
+    slide();
+})
+
+document.getElementById('bt-prox').addEventListener('click', function () {
+    atual++;
+    slide();
+})
+function slide() {
+    if (atual >= images.length) {
+        atual = 0;
+    } else if (atual < 0) {
+        atual = images.length - 1;
+    }
+    document.querySelector('.imgAtual').classList.remove('.imgAtual');
+    //define o tamanho da marginLeft de acordo com o tamanho da tela
+    
+    if (window.matchMedia("(min-width:1600px)").matches) {
+        firstImg.style.marginLeft = -1100 * atual + 'px';
+    } else if (window.matchMedia("(min-width:1360px)").matches) {
+        firstImg.style.marginLeft = -1000 * atual + 'px';
+    } else if (window.matchMedia("(max-width:425px)").matches) {
+        firstImg.style.marginLeft = -425 * atual + 'px';
+    }
+
+    document.getElementById('primeiraImg').classList.add('.imgAtual');
+}
+slide();
