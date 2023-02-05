7317c8fd968526419f65151a6c0f986fc103de05
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-02-05 20:05:42 -0300

    ajustando img do banner 1 no modo responsivo

--- Projeto Loja/src/js/app.js
+++ Projeto Loja/src/js/app.js
@@ -39,6 +39,7 @@ function responsive(){
     if(window.matchMedia("(max-width:600px)").matches){
         document.querySelector('.container-box').classList.remove('d-flex');
         document.querySelector('.footer-container').classList.remove('d-flex');
+        
     }
 }
 responsive();
