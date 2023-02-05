14e30a4c04d653c545ccdbd6f01921fe13d4fd73
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-02-05 19:02:37 -0300

    ajustando d flex modo responsivo via js

--- Projeto Loja/src/js/app.js
+++ Projeto Loja/src/js/app.js
@@ -37,7 +37,8 @@ slide();
 /*reponsive container boxes*/ 
 function responsive(){
     if(window.matchMedia("(max-width:600px)").matches){
-        document.querySelector('.d-flex').classList.remove('d-flex');
+        document.querySelector('.container-box').classList.remove('d-flex');
+        document.querySelector('.footer-container').classList.remove('d-flex');
     }
 }
 responsive();
