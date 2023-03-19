17323b93e92ad9a0640e82c7765a39cb23591ecd
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-03-19 17:39:36 -0300

    excluindo slide de imagens

--- Projeto Loja/src/js/app.js
+++ Projeto Loja/src/js/app.js
@@ -1,38 +1,3 @@
-/*Slide imagem*/
-
-let atual = 0;
-let images = document.querySelectorAll('.slides .imgs');
-let firstImg = document.getElementById('primeiraImg');
-
-document.getElementById('bt-voltar').addEventListener('click', function () {
-    atual--;
-    slide();
-})
-
-document.getElementById('bt-prox').addEventListener('click', function () {
-    atual++;
-    slide();
-})
-function slide() {
-    if (atual >= images.length) {
-        atual = 0;
-    } else if (atual < 0) {
-        atual = images.length - 1;
-    }
-    //document.querySelector('.imgAtual').classList.remove('imgAtual');
-    //define o tamanho da marginLeft de acordo com o tamanho da tela
-    
-    if (window.matchMedia("(min-width:1600px)").matches) {
-        firstImg.style.marginLeft = -1100 * atual + 'px';
-    } else if (window.matchMedia("(min-width:1360px)").matches) {
-        firstImg.style.marginLeft = -1000 * atual + 'px';
-    } else if (window.matchMedia("(max-width:600px)").matches) {
-        firstImg.style.marginLeft = -600 * atual + 'px';
-    }
-
-    document.getElementById('primeiraImg').classList.add('.imgAtual');
-}
-slide();
 
 /*reponsive container boxes*/ 
 function responsive(){
